ae63e99fb505aba2b65746e3e1950274b7268d8e
WilsonAEscobar <wilsonescobar909@outlook.com>
2023-10-27 20:00:23 -0500

    Login API working
    forgot to upload these

--- SchedulingBackend/api/urls.py
+++ SchedulingBackend/api/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views #imports main function from .views in api folder
 
 urlpatterns = [
+   path('register', views.UserRegister.as_view(), name='register'),
    path('login', views.UserLogin.as_view(), name='login'),
 	path('logout', views.UserLogout.as_view(), name='logout'),
 ]
--- SchedulingBackend/api/views.py
+++ SchedulingBackend/api/views.py
@@ -2,11 +2,20 @@ from django.contrib.auth import login, logout
 from rest_framework.authentication import SessionAuthentication
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .serializers import UserLoginSerializer
+from .serializers import UserLoginSerializer, UserRegisterSerializer
 from rest_framework import permissions, status
-from .validations import validate_email, validate_password
+from .validations import validate_email, validate_password,custom_validation
 
-
+class UserRegister(APIView):
+	permission_classes = (permissions.AllowAny,)
+	def post(self, request):
+		clean_data = custom_validation(request.data)
+		serializer = UserRegisterSerializer(data=clean_data)
+		if serializer.is_valid(raise_exception=True):
+			user = serializer.create(clean_data)
+			if user:
+				return Response(serializer.data, status=status.HTTP_201_CREATED)
+		return Response(status=status.HTTP_400_BAD_REQUEST)
 
 
 class UserLogin(APIView):
